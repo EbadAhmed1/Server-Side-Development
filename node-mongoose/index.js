3490e512c81c88cacb5af361578dc2ec74d92cdf
Ebad Ahmed <ebadahmed20005@gmail.com>
2025-07-17 23:08:27 +0500

    Mongoose 1

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -1,18 +1,16 @@
 const mongoose = require('mongoose');
 const Dishes = require('./models/dishes');
 
-const url = 'mongodb://localhost:27017/conFusion'; // Update with your connection string
+const url = 'mongodb://localhost:27017/conFusion'; 
 const connect = mongoose.connect(url);
 
 connect.then((db) => {
     console.log('Connected correctly to server');
     
-    const newDish = new Dishes({
+    Dishes.create({
         name: 'pizza',
         description: 'test'
-    });
-
-    return newDish.save()
+    })
         .then((dish) => {
             console.log(dish);
             return Dishes.find({}).exec();
